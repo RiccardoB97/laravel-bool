66d8facfd6e2bf67f67980504cc7d2d6799e6f75
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-22 11:55:51 +0200

    Implement posts edit and update methods

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -10,22 +10,23 @@
         </ul>
     </div>
 @endif
-<form action="{{route('admin.posts.store')}}" method="post">
+<form action="{{route('admin.posts.update', $post->id)}}" method="post">
     @csrf
+    @method('PUT')
     <div class="form-group">
       <label for="title">Title</label>
-      <input type="text" name="title" id="title" class="form-control" placeholder="add a title" aria-describedby="titleHelper" value="{{old('title')}}">
+      <input type="text" name="title" id="title" class="form-control" placeholder="add a title" aria-describedby="titleHelper" value="{{$post->title}}">
       <small id="titleHelper" class="text-muted">Type a title for the current post</small>
     </div>
     <div class="form-group">
         <label for="image">Cover Image</label>
-        <input type="text" name="image" id="image" class="form-control" placeholder="https://" aria-describedby="imageHelper" value="{{old('image')}}">
+        <input type="text" name="image" id="image" class="form-control" placeholder="https://" aria-describedby="imageHelper" value="{{$post->image}}">
         <small id="imageHelper" class="text-muted">Type an image URL for the current post</small>
     </div>
     <div class="form-group">
         <label for="content">Content</label>
-        <textarea class='form-control' name="content" id="content" rows='4'>{{old('content')}}</textarea>
+        <textarea class='form-control' name="content" id="content" rows='4' >{{$post->content}}</textarea>
     </div>
-      <button type="submit" class="btn btn-success">Submit</button>
+      <button type="submit" class="btn btn-success">Update</button>
 </form>
 @endsection
