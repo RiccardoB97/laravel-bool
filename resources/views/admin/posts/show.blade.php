83370382b5fdb483114b1f4150fb7abc8f031e10
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-07-22 11:04:48 +0200

    Add show for posts

---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+<img src="{{$post->image}}" alt="">
+<h1>{{$post->title}}</h1>
+<p>{{$post->content}}</p>
+
+<a href="{{route('admin.posts.index')}}"><i class="fas fa-angle-left"> Back</i></a>
+<a href="{{route('admin.posts.edit', $post->id)}}">Edit</a>
+
+@endsection
